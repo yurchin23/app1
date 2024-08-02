d639ed62886e883539a90ea1deb2f11176a00875
Daniil Yurchyn <yurchin2398@gmail.com>
2024-08-03 01:10:18 +0300

    ref

--- spring-boot-app/app1/setup.sh
+++ spring-boot-app/app1/setup.sh
@@ -2,4 +2,4 @@ set -e
 mvn clean package
 docker build .  -t myapp1
 docker tag myapp1:latest localhost:5000/myapp1:latest
-docker push localhost:5000/myapp:latest
+docker push localhost:5000/myapp1:latest
