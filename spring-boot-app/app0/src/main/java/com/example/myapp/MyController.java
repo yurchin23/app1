6ee4afa713f9073031e4b0b0246af960effd5492
Daniil Yurchyn <yurchin2398@gmail.com>
2024-08-10 15:10:38 +0300

    auth policy & ref app0

--- spring-boot-app/app0/src/main/java/com/example/myapp/MyController.java
+++ spring-boot-app/app0/src/main/java/com/example/myapp/MyController.java
@@ -1,12 +1,17 @@
 package com.example.myapp;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.view.RedirectView;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MyController {
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     @GetMapping("/")
     public String home() {
@@ -14,8 +19,11 @@ public class MyController {
     }
 
     @GetMapping("/send")
-    public RedirectView send(@RequestParam(value = "name") String name) {
-        String url = "https://localhost.app1.xyz/api/greet?name=" + name;
-        return new RedirectView(url);
+    public ModelAndView send(@RequestParam(value = "name") String name) {
+        String url = "http://myapp1-service/api/greet?name=" + name;
+        String response = restTemplate.getForObject(url, String.class);
+        ModelAndView modelAndView = new ModelAndView("result");
+        modelAndView.addObject("message", response);
+        return modelAndView;
     }
 }
